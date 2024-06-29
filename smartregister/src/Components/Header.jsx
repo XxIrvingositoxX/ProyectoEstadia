f33437bd44acae8eb2faf1d9f354506ce006d524
Irving <martinezlopezirving562@gmail.com>
2024-06-29 15:46:41 -0500

    Avance de vista Colaborador

--- smartregister/src/Components/Header.jsx
+++ smartregister/src/Components/Header.jsx
@@ -20,6 +20,7 @@ import {
   FingerPrintIcon,
   SquaresPlusIcon,
   XMarkIcon,
+  XCircleIcon
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
@@ -46,9 +47,8 @@ export default function Header() {
     <header className="bg-white drop-shadow-xl">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-14 w-auto" src={Logo} alt="" />
+          <a href="#" className="-m-1.5 p-4">
+            <img className="h-14 w-auto left-24" src={Logo} alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -61,64 +61,23 @@ export default function Header() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <Popover className="relative">
-            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Product
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-            </PopoverButton>
-
-            <PopoverPanel
-              transition
-              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-              <div className="p-4">
-                {products.map((item) => (
-                  <div
-                    key={item.name}
-                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                  >
-                    <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                      <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                    </div>
-                    <div className="flex-auto">
-                      <a href={item.href} className="block font-semibold text-gray-900">
-                        {item.name}
-                        <span className="absolute inset-0" />
-                      </a>
-                      <p className="mt-1 text-gray-600">{item.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                {callsToAction.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                  >
-                    <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </PopoverPanel>
-          </Popover>
-
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+        <PopoverGroup className="hidden lg:flex lg:gap-x-24">
+        <a href="#" className="text-base font-semibold leading-6 text-gray-900 hover:underline underline-offset-4">
+            Inicio
+          </a>
+          <a href="#" className="text-base font-semibold leading-6 text-gray-900 hover:underline underline-offset-4">
             Colaboradores
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-base font-semibold leading-6 text-gray-900 hover:underline underline-offset-4">
             Visitantes
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-base font-semibold leading-6 text-gray-900 hover:underline underline-offset-4">
             Llaves
           </a>
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+          <a href="#" className="text-base font-semibold leading-6 text-red-600 hover:text-red-800 hover:underline underline-offset-4">
+            Cerrar sesión <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
       </nav>
@@ -149,28 +108,16 @@ export default function Header() {
                 <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
                     <>
-                      <DisclosureButton className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Product
-                        <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                          aria-hidden="true"
-                        />
-                      </DisclosureButton>
-                      <DisclosurePanel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <DisclosureButton
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </DisclosureButton>
-                        ))}
-                      </DisclosurePanel>
+                      
                     </>
                   )}
                 </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Inicio
+                </a>
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
@@ -193,9 +140,9 @@ export default function Header() {
               <div className="py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="text-base font-semibold leading-6 text-red-600 hover:text-red-800"
                 >
-                  Log in
+                  Cerrar sesión
                 </a>
               </div>
             </div>
--- smartregister/src/Pages/Collegue.jsx
+++ smartregister/src/Pages/Collegue.jsx
@@ -1,9 +1,90 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../Components/Header";
+import { Table, Button } from "flowbite-react";
 
-function Collegue(){
-    return(
-        <Header/>
-    )
+function Collegue() {
+    useEffect(() => {
+        document.title = "Colaboradores";
+    }, []);
+    return (
+        <>
+            <Header />
+            <p class="font-sans text-5xl flex justify-center mt-10 font-medium tracking-wide">Colaboradores</p>
+            <form class="max-w-md mx-auto mt-24 ml-40">
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Colaborador..." required />
+                    <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-sky-800 hover:bg-sky-900 font-medium rounded-lg text-sm px-4 py-2">Buscar</button>
+                </div>
+            </form>
+
+            <div className="flex justify-center mt-14">
+                <div className="overflow-x-auto w-5/6 mx-auto">
+                    <Table hoverable className="min-w-full text-xs">
+                        <Table.Head className="text-lg">
+                            <Table.HeadCell className="p-2">No.Colaborador</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Nombre</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Departamento</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Fecha</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Entrada</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Salida</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Estado</Table.HeadCell>
+                            <Table.HeadCell className="p-2">Acción</Table.HeadCell>
+                        </Table.Head>
+                        <Table.Body className="divide-y text-base">
+                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white p-2">
+                                    12332
+                                </Table.Cell>
+                                <Table.Cell className="p-2">Gerardo Gomez Palacios</Table.Cell>
+                                <Table.Cell className="p-2">Ama de llaves</Table.Cell>
+                                <Table.Cell className="p-2">29/06/2024</Table.Cell>
+                                <Table.Cell className="p-2">10:24:23</Table.Cell>
+                                <Table.Cell className="p-2">6:22:32</Table.Cell>
+                                <Table.Cell className="p-2">Fuera</Table.Cell>
+                                <Table.Cell className="p-2">
+                                    <Button className="bg-green-500 hover:bg-green-700 pr-4 pl-4">Entrada</Button>
+                                </Table.Cell>
+                            </Table.Row>
+                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white p-2">
+                                    12332
+                                </Table.Cell>
+                                <Table.Cell className="p-2">Gerardo Gomez Palacios</Table.Cell>
+                                <Table.Cell className="p-2">Ama de llaves</Table.Cell>
+                                <Table.Cell className="p-2">29/06/2024</Table.Cell>
+                                <Table.Cell className="p-2">10:24:23</Table.Cell>
+                                <Table.Cell className="p-2">6:22:32</Table.Cell>
+                                <Table.Cell className="p-2">Dentro</Table.Cell>
+                                <Table.Cell className="p-2">
+                                    <Button className="bg-red-700 hover:bg-red-800 pr-5 pl-5">Salida</Button>
+                                </Table.Cell>
+                            </Table.Row>
+                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white p-2">
+                                    12332
+                                </Table.Cell>
+                                <Table.Cell className="p-2">Gerardo Gomez Palacios</Table.Cell>
+                                <Table.Cell className="p-2">Ama de llaves</Table.Cell>
+                                <Table.Cell className="p-2">29/06/2024</Table.Cell>
+                                <Table.Cell className="p-2">10:24:23</Table.Cell>
+                                <Table.Cell className="p-2">6:22:32</Table.Cell>
+                                <Table.Cell className="p-2">Fuera</Table.Cell>
+                                <Table.Cell className="p-2">
+                                    <Button className="bg-green-500 hover:bg-green-700 pr-4 pl-4">Entrada</Button>
+                                </Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                    </Table>
+                </div>
+            </div>
+        </>
+    );
 }
+
 export default Collegue;
